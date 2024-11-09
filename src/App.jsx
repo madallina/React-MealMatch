bdd5e360711f5097728cff485ab1f2eff8f4682e
madallina <goncearucmadalina@yahoo.com>
2024-11-09 15:34:17 +0200

    feat: function getMealRecipe

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const [error, setError] = useState('');
 const getMealList = async(ingredient) =>{
   try{
     const response = await fetch(
-      'https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}'
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
     );
     const data = await response.json();
     if(data.meals){
@@ -29,6 +29,18 @@ const getMealList = async(ingredient) =>{
     setMeals([]);
     setError("Sorry , something went wrong!");
   }
+};
+
+const getMealRecipe = async(id)=>{
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    setMealDetails(data.meals[0]);
+  } catch (error){
+    console.error("Error fetching meal recipe:", error);
+
+  }
+  
 };
   return (
     <>
