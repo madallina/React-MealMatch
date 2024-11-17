e82036dfbd4e405f3e92dac7da2fb5dff27fdc6b
madallina <goncearucmadalina@yahoo.com>
2024-11-17 11:00:54 +0200

    feat: MealItem component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,6 +1,20 @@
 
 const MealItem = ({meal, onClick})=>{
 
+return (
+<div className="meal-tem" data-id={meal.idMeal} onClick={()=>onClick(meal.idMeal)}>
+    <div className="meal-img">
+        <img src={meal.strMealThumb} alt={meal.strMeal} />
+
+    </div>
+    <div className="meal-name">
+        <h3>{meal.strMeal}</h3>
+        <button className="recipe-btn">Get recipe</button>
+
+    </div>
+
+</div>
+);
 };
 
 export default MealItem ;
